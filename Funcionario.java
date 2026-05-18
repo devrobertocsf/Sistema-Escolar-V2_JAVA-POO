4862cf98ec9bd74a54fa16ce23430a7c36c1733f
devrobertocsf <robertocarlosw5251@gmail.com>
2026-05-18 11:54:53 -0300

    código

---
+++ Funcionario.java
@@ -0,0 +1,31 @@
+package pacote10;
+
+public class Funcionario extends Pessoa {
+
+private String setor;
+private boolean trabalhando;
+
+// método funcionario
+
+public void mudarTrabalho() {
+	
+}
+
+//métodos especiais
+
+public String getSetor() {
+	return setor;
+}
+public void setSetor(String setor) {
+	this.setor = setor;
+}
+public boolean isTrabalhando() {
+	return trabalhando;
+}
+public void setTrabalhando(boolean trabalhando) {
+	this.trabalhando = trabalhando;
+}
+
+
+	
+}
